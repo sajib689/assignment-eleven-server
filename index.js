e9041ff6e81afdd88ac16e8e6bbc71b7ed8165db
sajib689 <jibonraja11@gmail.com>
2023-09-24 19:58:29 +0600

    data change

--- index.js
+++ index.js
@@ -50,16 +50,26 @@ async function run() {
       res.send(result)
     })
     // get reviews
+    app.get('/reviews/:serviceId', async (req, res) => {
+      const serviceId = req.params.serviceId 
+      const cursor = {serviceId: (serviceId)}
+      console.log(cursor)
+      let query = {}
+      if(req.query?.email) {
+        query = {email: req.query.email}
+      }
+       const result = await reviewsCollection.find(query,cursor).toArray()
+       res.send(result)
+    })
+    // get all reviews
     app.get('/reviews', async (req, res) => {
       let query = {}
       if(req.query?.email) {
         query = {email: req.query.email}
       }
-       const result = await reviewsCollection.find(query).toArray()
+      const result = await reviewsCollection.find(query).toArray()
        res.send(result)
     })
-    // get all reviews
-    app.get
     // get single review
     app.get('/reviews/:id', async (req, res) => {
       const id = req.params.id
